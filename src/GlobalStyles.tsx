08879743b9b33376c9fec61a7f814187fdd474eb
Sadnan Saquif <ssaquif@gmail.com>
2024-02-09 18:30:19 -0500

    styling setup

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,17 @@
+import { globalCss } from "@stitches/react";
+
+const globalStyles = globalCss({
+  // Use dvh/dvw & use percentage-based heights as fallback (need to test if fallback works)
+  "html,body": {
+    height: `100%; height: 100dvh`,
+    width: `100%; width: 100dvw`,
+  },
+  // inherit height and width from parent
+  "#root": {
+    height: "inherit",
+    width: "inherit",
+    // border: "2px solid red",
+  },
+});
+
+export default globalStyles;
