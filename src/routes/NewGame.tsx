ae6c0b57706b7ab10ad44f6a61fa228b4d17d4ae
Sadnan Saquif <ssaquif@gmail.com>
2024-02-27 23:09:24 -0500

    basic barebone toast

--- src/routes/NewGame.tsx
+++ src/routes/NewGame.tsx
@@ -1,9 +1,65 @@
 import { Form, useNavigate } from "react-router-dom";
+import * as Toast from "@radix-ui/react-toast";
 import { styled } from "../stitches-theme";
 import BaseButton from "../components/Button";
+import { useState } from "react";
+
+type NewGameEntry = {
+  username: string;
+  numOfPlayers: number;
+};
+
+type EntryError = {
+  isError: boolean;
+  message: string | null;
+};
 
 function NewGame() {
   const navigate = useNavigate();
+  const [newGameEntry, setNewGameEntry] = useState<NewGameEntry>({
+    username: "",
+    numOfPlayers: 2,
+  });
+  const [error, setError] = useState<EntryError>({
+    isError: false,
+    message: null,
+  });
+
+  function validateUserData() {
+    // todo add regex validation for username, to only contain letters and numbers
+
+    if (
+      newGameEntry?.username.length < 3 ||
+      newGameEntry?.username.length > 20
+    ) {
+      setError({
+        isError: true,
+        message: "Username must be between 3-20 characters",
+      });
+      return false;
+    }
+    if (newGameEntry?.username.match(/[^a-zA-Z0-9]/)) {
+      setError({
+        isError: true,
+        message: "Username must contain only letters and numbers",
+      });
+      return false;
+    }
+    if (newGameEntry?.numOfPlayers < 2 || newGameEntry?.numOfPlayers > 6) {
+      setError({
+        isError: true,
+        message: "Number of players must be between 2-6",
+      });
+      return false;
+    }
+  }
+
+  function handleCreateGame() {
+    console.log("newGameEntry", newGameEntry);
+    if (!validateUserData()) {
+      return;
+    }
+  }
 
   function handleCancel() {
     navigate("/home");
@@ -11,32 +67,69 @@ function NewGame() {
 
   return (
     <Container>
-      <StyledForm>
-        <AvatarContainer>
-          {/* todo: replace with proper selectable avatar component */}
-          <div
-            style={{
-              height: "100px",
-              width: "100px",
-              borderRadius: "50%",
-              backgroundColor: "var(--gray-300)",
+      <Toast.Provider swipeDirection="right">
+        <StyledForm onSubmit={handleCreateGame}>
+          <AvatarContainer>
+            {/* todo: replace with proper selectable avatar component */}
+            <div
+              style={{
+                height: "100px",
+                width: "100px",
+                borderRadius: "50%",
+                backgroundColor: "var(--gray-300)",
+              }}
+            ></div>
+          </AvatarContainer>
+          <FormDataRowContainer>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <FormInput
+              type="text"
+              id="username"
+              value={newGameEntry.username}
+              onChange={(ev) => {
+                setNewGameEntry((prev) => ({
+                  ...prev,
+                  username: ev.target.value,
+                }));
+              }}
+            />
+          </FormDataRowContainer>
+          <FormDataRowContainer>
+            <FormLabel htmlFor="numOfPlayers">Players</FormLabel>
+            {/* Write validation yourself */}
+            <FormInput
+              type="text"
+              id="numOfPlayers"
+              value={newGameEntry.numOfPlayers}
+              onChange={(ev) => {
+                setNewGameEntry((prev) => ({
+                  ...prev,
+                  numOfPlayers: parseInt(ev.target.value),
+                }));
+              }}
+            />
+          </FormDataRowContainer>
+          <ButtonContainer>
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Button type="submit">Create</Button>
+          </ButtonContainer>
+        </StyledForm>
+        {/* // Make a separate reusable toast component */}
+        <Toast.Root open={error.isError}>
+          <Toast.Title>{error.message}</Toast.Title>
+          <Toast.Close
+            onClick={() => {
+              setError({
+                isError: false,
+                message: null,
+              });
             }}
-          ></div>
-        </AvatarContainer>
-        <FormDataRowContainer>
-          <FormLabel htmlFor="username">Username</FormLabel>
-          <FormInput type="text" id="username" />
-        </FormDataRowContainer>
-        <FormDataRowContainer>
-          <FormLabel htmlFor="numOfPlayers">Players</FormLabel>
-          {/* Write validation yourself */}
-          <FormInput type="text" id="numOfPlayers" />
-        </FormDataRowContainer>
-        <ButtonContainer>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button type="submit">Create</Button>
-        </ButtonContainer>
-      </StyledForm>
+          >
+            Close
+          </Toast.Close>
+        </Toast.Root>
+        <ToastViewport />
+      </Toast.Provider>
     </Container>
   );
 }
@@ -93,6 +186,18 @@ const ButtonContainer = styled("div", {
   justifyContent: "center",
 });
 
+const ToastViewport = styled(Toast.Viewport, {
+  position: "fixed",
+  bottom: 0,
+  left: "50%",
+  transform: `translate(-50%, -50%)`,
+  padding: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid var(--gray-300)",
+});
 // todo: maybe not needed just use BaseButton?
 const Button = styled(BaseButton, {});
 
